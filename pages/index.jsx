f25c1010d4ab120320120a8f8d5eecabea823fa3
andyjxck <andyblewett9@gmail.com>
2025-10-24 01:49:40 +0100

    Update logo image source in index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -187,7 +187,7 @@ async function runSequence() {
         <div className="zen-container">
           <header className="zen-header">
             <div className="zen-logo">
-  <img src="https://ibb.co/1tvbzzNR" alt="Zen Void Logo" className="zen-logo-img" />
+  <img src="https://i.ibb.co/Vcj3wwGM/Chat-GPT-Image-Oct-23-2025-03-43-00-PM.png" alt="Zen Void Logo" className="zen-logo-img" />
 </div>
             <div>
               <h1 className="zen-title">Zen Void</h1>
